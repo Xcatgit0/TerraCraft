04a128f17b907df4c4d60d96bde7a69d8755e4b5
= <umapornachira@gmail.com>
2024-10-29 19:41:53 +0700

    thins!

--- Player.js
+++ Player.js
@@ -3,10 +3,8 @@ function create() {
     const element = document.getElementById("control");
     const buttonUp = document.createElement("button");
     buttonUp.innerText = "^";
-    const pox = window.innerHeight - 30;
-    const posx = String(pox);
-    posx.concat("px");
-    buttonUp.style.top = posx;
+    buttonUp.id = "moveUp";
+    //buttonUp.style.top = posx;
     
 
     element.appendChild(buttonUp);
