3b827d0f2b73c55e2788e43eb41a9ab77cceea6f
= <umapornachira@gmail.com>
2024-10-29 19:45:13 +0700

    fixed

--- Player.js
+++ Player.js
@@ -3,7 +3,7 @@ function create() {
     const element = document.getElementById("control");
     const buttonUp = document.createElement("button");
     buttonUp.innerText = "^";
-    buttonUp.id = "moveUp";
+    
     //buttonUp.style.top = posx;
     
 
